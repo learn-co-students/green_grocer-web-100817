4e4e97d5dd75e7caf19f99a78111267ff8134c3b
Lina Rudashevski <lina.rudashevski@gmail.com>
2017-09-27 16:38:11 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,65 @@
+
+
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |data, attribute|
+    data.each do |ingredient, hashname|
+      new_hash[ingredient] = hashname
+      new_hash[ingredient][:count] = 0
+      #data = {"AVOCADO" => {:price => 3.0, :clearance => true }
+      #ingredient = "AVOCADO"
+      #hashname = {:price => 3.0, :clearance => true }
+end
+end
+   cart.each do |data,attribute|
+     data.each do |ingredient, hashname|
+       if new_hash.include?(ingredient)
+         new_hash[ingredient][:count] += 1
+       end
+     end
+   end
+new_hash
 end
 
+
 def apply_coupons(cart, coupons)
-  # code here
+  array = []
+  new_hash = {}
+    cart.each do |ingredient, hashname|
+      coupons.each do |hash|
+        hash.each do |key,value|
+          if value == ingredient && cart[ingredient][:count] >= hash[:num]
+            array << ingredient
+            array = array.select {|x| x == ingredient}
+             cart[ingredient][:count] -= hash[:num]
+       new_hash["#{ingredient} W/COUPON"]= {:price => hash[:cost], :clearance => cart[ingredient][:clearance], :count => array.length}
+     end
+   end
+ end
+ end
+cart.merge(new_hash)
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |ingredient, hashname|
+    if cart[ingredient][:clearance] == true
+      cart[ingredient][:price] = (cart[ingredient][:price] * 0.8).round(1)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+    cart = consolidate_cart(cart)
+    cart = apply_coupons(cart, coupons)
+    cart = apply_clearance(cart)
+    sum = 0
+    cart.each do |ingredient, hashname|
+      sum += (hashname[:price] * hashname[:count])
+    end
+
+    if sum > 100
+      sum = sum * 0.9
+    end
+    sum
 end
